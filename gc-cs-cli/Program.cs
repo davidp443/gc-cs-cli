55266b0609d7f630cbd18aebec21a0d7c76b18a5
David Perez <david@perez.gq>
2021-09-12 22:05:16 +0100

    List all customers

--- gc-cs-cli/Program.cs
+++ gc-cs-cli/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using GoCardless;
+using GoCardless.Resources;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace gc_cs_cli
 {
@@ -6,7 +10,21 @@ namespace gc_cs_cli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+
+            var accessToken = "sandbox_xXXXXxx";
+            var gocardless = GoCardlessClient.Create(accessToken, GoCardlessClient.Environment.SANDBOX);
+
+            IEnumerable<Customer> customers = gocardless.Customers.All();
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Id);
+
+            }
         }
     }
 }
